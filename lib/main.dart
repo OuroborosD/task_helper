b0df1c2d414a5d0477aa6c66cf0f528892f8f1c2
OroborosDm <diego-siebra@hotmail.com>
2022-08-05 16:51:42 -0300

    criado as pastas

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:task_helper/app/views/pages/home_page.dart';
+
+void main() => runApp(Main());
+
+
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
